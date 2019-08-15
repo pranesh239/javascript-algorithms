277ed8989c9cb953b76150779c982be8325a1d26
Pranesh.g <praneshpsg239@gmail.com>
2019-08-15 21:34:01 +0530

    Update | DS - Tree - Breadth first traversal

---
+++ DS - Tree/Tree.js
@@ -0,0 +1,37 @@
+/*
+Implementation of Tree data structure.
+*/
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter(child => child.data !== data);
+  }
+}
+
+class Tree {
+  constructor(root) {
+    this.root = root;
+  }
+
+  traverseBreadthFirst(fn) {
+    let currentNode = [this.root];
+
+    while (currentNode.length) {
+      const currentEl = currentNode.shift();
+      currentNode.push(...currentEl.children);
+      fn(currentEl);
+    }
+  }
+}
+
+exports.Tree = Tree;
+exports.Node = Node;
---
+++ DS - Tree/Tree.test.js
@@ -0,0 +1,48 @@
+const { Node } = require("./Tree");
+const { Tree } = require("./Tree");
+
+describe("Tree", () => {
+  let n1;
+  beforeEach(() => {
+    n1 = new Node(1);
+  });
+
+  it("should contain a node with no children", () => {
+    expect(n1.data).toBe(1);
+    expect(n1.children).toHaveLength(0);
+  });
+
+  it("should contain a node with one children", () => {
+    n1.add(2);
+    expect(n1.children).toHaveLength(1);
+  });
+
+  it("should remove a child from tree when remove operation is performed", () => {
+    n1.add(2);
+    expect(n1.children).toHaveLength(1);
+    n1.remove(2);
+    expect(n1.children).toHaveLength(0);
+  });
+
+  it("should traverse thr given tree in Breadth First manner and should execute the given function on every node", () => {
+    n1.add(2);
+    n1.add(3);
+    n1.add(4);
+    const t1 = new Tree(n1);
+    t1.traverseBreadthFirst(node => {
+      node.data = node.data + 10;
+    });
+    const expectedData = {
+      root: {
+        data: 11,
+        children: [
+          { data: 12, children: [] },
+          { data: 13, children: [] },
+          { data: 14, children: [] }
+        ]
+      }
+    };
+
+    expect(t1).toEqual(expectedData);
+  });
+});
